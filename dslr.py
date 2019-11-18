19c82aff9d9fce7acc1eabe5e0b2de03edbb3185
mhtsbt <matthias.hutsebaut@gmail.com>
2019-11-18 21:21:09 +0100

    refactor structure

--- dslr.py
+++ dslr.py
@@ -1,36 +1,44 @@
 from fpdf import FPDF
 import subprocess
 import cv2
+import time
+from shutil import copyfile
+
+IMG_TEMP_FILEN = "capt00001.jpg"
+OUTPUT_PDF = "out.pdf"
 
 def take_picture():
-
 	process = subprocess.Popen("gphoto2 --capture-image-and-download --force-overwrite", shell=True, stdout=subprocess.PIPE)
 	process.wait()
 	print(process.returncode)
+	print("Picture taken")
 
-#take_picture()
+def store_picture():
+	copyfile(IMG_TEMP_FILEN, f"./pictures/{time.time()}.jpg")
 
-def resize_picture(fname):
-	img = cv2.imread(fname)
+def resize_picture():
+	img = cv2.imread(IMG_TEMP_FILEN)
 	img = cv2.resize(img, None, fx=0.2, fy=0.2)
-	cv2.imwrite("test2.jpg", img)
+	cv2.imwrite("out.jpg", img)
 
-def generate_pdf(filename):
+def generate_pdf():
 
     pdf = FPDF('P', 'mm', (100, 150))
     pdf.add_page()
 #    pdf.set_font('Arial', 'B', 16)
 #    pdf.cell(40, 5, 'MATTHIAS & CELINE')
-    pdf.image(name=filename, x =5, y = 20, w = 90, h = 70, link = filename)
+    pdf.image(name="out.jpg", x =5, y = 20, w = 90, h = 70, link = "out.jpg")
 
-    pdf.output("out.pdf", 'F')
+    pdf.output(OUTPUT_PDF, 'F')
 
     print("pdf ready")
 
 def print_pdf():
-	print (subprocess.check_output(['lp', "out.pdf"]))
+	print (subprocess.check_output(['lp', OUTPUT_PDF]))
 
 
-resize_picture("test.jpg")
-generate_pdf("test2.jpg")
+take_picture()
+store_picture()
+resize_picture()
+generate_pdf()
 print_pdf()
